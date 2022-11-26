b2e50d567476a5a877621104075fddf8ab3ef6a9
Victor V. Vu <78053016+vuvictor1@users.noreply.github.com>
2022-11-25 18:37:05 -0800

    Update Exitmain.cs

--- Exitmain.cs
+++ Exitmain.cs
@@ -1,25 +1,25 @@
-//******************************************************************************
-// Author Information:
-// Name: Victor V. Vu
-// Email: vuvictor@csu.fullerton.edu
-//
-// Program Information:
-// Program Name: Colored Exit Sign
-// This File: Exitmain.cs
-// Description: Manager file that will call on UI.cs to display graphics
-//******************************************************************************
-// Copyright (C) 2022 Victor V. Vu
-// This program is free software: you can redistribute it and/or modify it under
-// the terms of the GNU General Public License version 3 as published by the
-// Free Software Foundation. This program is distributed in the hope that it
-// will be useful, but WITHOUT ANY WARRANTY without even the implied Warranty of
-// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General
-// Public License for more details. A copy of the GNU General Public License v3
-// is available here: <https://www.gnu.org/licenses/>.
-//******************************************************************************
-// Programmed in Ubuntu-based Linux Platform.
-// To run program, type in terminal: "sh r.sh"
-//******************************************************************************
+/*******************************************************************************
+   Author Information:
+   Name: Victor V. Vu
+   Email: vuvictor@csu.fullerton.edu
+
+   Program Information:
+   Program Name: Colored Exit Sign
+   This File: Exitmain.cs
+   Description: Manager file that will call on UI.cs to display graphics
+
+   Copyright (C) 2022 Victor V. Vu
+   This program is free software: you can redistribute it and/or modify it under
+   the terms of the GNU General Public License version 3 as published by the
+   Free Software Foundation. This program is distributed in the hope that it
+   will be useful, but WITHOUT ANY WARRANTY without even the implied Warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General
+   Public License for more details. A copy of the GNU General Public License v3
+   is available here: <https://www.gnu.org/licenses/>.
+
+   Programmed in Ubuntu-based Linux Platform.
+   To run program, type in terminal: "sh r.sh"
+  ******************************************************************************/
 
 // Inlcudes the required system files
 using System;
